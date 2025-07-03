795fbe0198b0dd9490428097b55442fda2a8f8d7
betterwork7777 <kiontroy@gmail.com>
2025-07-02 18:57:36 -0700

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Common Sense Doc Services - Coming Soon!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
